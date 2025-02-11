de1bfaf9eb54002fcb4c69bf01e98aca0ec9da2b
SaaiThirumu14 <148644164+SaaiThirumu14@users.noreply.github.com>
2025-02-11 18:39:27 +0530

    Update connectDatabase.js

--- config/connectDatabase.js
+++ config/connectDatabase.js
@@ -6,7 +6,10 @@ const { default: mongoose } = require("mongoose");
 })(for a function case)*/
 
 const connectDatabase=()=>{
-    mongoose.connect(process.env.DB_URL).then(()=>{
+    mongoose.connect(process.env.DB_URL,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(()=>{
         console.log(`db connected`);
     })
 }
