7ebc83841b08abec61fa9dc0fbe01aa93c9737cd
SaaiThirumu14 <148644164+SaaiThirumu14@users.noreply.github.com>
2025-02-11 17:38:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ app.use(express.json())//for inserting a data
 //express.json gets the data from the front end and it will be come as a json data is denoted by this function
 app.use(`/api`,product);
 app.use(`/api`,orders);
+app.get("/", (req, res) => {
+  res.send("the server is working");
+});
 app.listen(process.env.PORT,()=>{
     console.log(`hello my server ${process.env.PORT}`)
 })
